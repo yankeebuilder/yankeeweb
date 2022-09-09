7f85f2c5037d893b3a2c4e4f84c90889ba0def87
ngdream <nguewofoss@gmail.com>
2022-09-09 20:37:14 +0100

    alpha version lauching

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-
-
   <RouterView />
 </template>
 
